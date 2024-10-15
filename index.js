8959326286a6ea19c669bcbd3184445fbe554026
rafael my master <sorafaelbook@gmail.com>
2024-10-14 22:32:28 -0500

    added test function

--- index.js
+++ index.js
@@ -7,104 +7,104 @@ let GText
 let Ggrid
 let GRect
 
-module.init = function(_GUI) {
+function bylonUIInit(_GUI) {
     G = _GUI
     GBtn = _GUI.Button
     GText = _GUI.TextBlock
     Ggrid = _GUI.Grid
     GRect = _GUI.Rectangle
     GCheckBx = _GUI.Checkbox
-    if(!ADT) ADT = G.AdvancedDynamicTexture("ADT_GUI")
+    if (!ADT) ADT = G.AdvancedDynamicTexture("ADT_GUI")
 }
-module.createBtn = function(btnLabel, _ADTexture, buttonImg, _width, _height, _fontS, _background){
+function createBtn(btnLabel, _ADTexture, buttonImg, _width, _height, _fontS, _background) {
     let btn
-    if(buttonImg){
+    if (buttonImg) {
         console.log("running")
         btn = GUI.Button.CreateImageButton("button", btnLabel, buttonImg)
-    }else btn = GUI.Button.CreateSimpleButton("button", btnLabel)
-    
-    
+    } else btn = GUI.Button.CreateSimpleButton("button", btnLabel)
+
+
     let defWidth = .6
     let defBackgroundC = "#27282a";
-    
+
     btn.width = _widthInNum ? _widthInNum : defWidth
-    btn.height = _height ? _height : defWidth/1.5
+    btn.height = _height ? _height : defWidth / 1.5
     btn.fontSize = _fontS ? _fontS : (defWidth * 90)
-    btn.background = _background?_background:defBackgroundC
+    btn.background = _background ? _background : defBackgroundC
     btn.color = "white"
 
-    if(_ADTexture) _ADTexture.addControl(btn)
+    if (_ADTexture) _ADTexture.addControl(btn)
     return btn
 }
-module.createRowsOfText = function(_arrayOfText, _fontS, parentUI){
+function createRowsOfText(_arrayOfText, _fontS, parentUI) {
     const grid = new Ggrid("grid")
     const textBlocks = []
-    let size = 1/_arrayOfText.length
+    let size = 1 / _arrayOfText.length
     _arrayOfText.forEach((txt, indx) => {
         const txtBlock = createTxt(txt, _fontS, "white")
         grid.addRowDefinition(size)
-        grid.addControl(txtBlock, indx,0)
-        if(_arrayOfText.length <= 3) grid.addRowDefinition(size)
+        grid.addControl(txtBlock, indx, 0)
+        if (_arrayOfText.length <= 3) grid.addRowDefinition(size)
 
         textBlocks.push(txt)
     })
     parentUI.addControl(grid)
-    return {grid, textBlocks}
+    return { grid, textBlocks }
 }
-module.createGrid = function(parentUI, rowsSizesArray, columnSizesArray){
+function createGrid(parentUI, rowsSizesArray, columnSizesArray) {
     const grid = new Ggrid("grid")
 
-    if(rowsSizesArray){
-        if(!rowsSizesArray.length) return log("rowSizeArray must be an array of sizes ex. [.1,.2,.5]")
-        rowsSizesArray.forEach( size => {
+    if (rowsSizesArray) {
+        if (!rowsSizesArray.length) return log("rowSizeArray must be an array of sizes ex. [.1,.2,.5]")
+        rowsSizesArray.forEach(size => {
             grid.addRowDefinition(size)
         })
     }
-    if(columnSizesArray){
-        if(!columnSizesArray.length) return log("columnSizesArray must be an array of sizes ex. [.1,.2,.5]")
-        columnSizesArray.forEach( size => {
+    if (columnSizesArray) {
+        if (!columnSizesArray.length) return log("columnSizesArray must be an array of sizes ex. [.1,.2,.5]")
+        columnSizesArray.forEach(size => {
             grid.addColumnDefinition(size)
         })
     }
-    if(parentUI) parentUI.addControl(grid) // could be a rectangle or another grid
+    if (parentUI) parentUI.addControl(grid) // could be a rectangle or another grid
     return grid
 }
-module.createRect = function(_childUI, _defWidthInNum,_defHeightInNum, _ADTexture, _cornerRadius,_background){
+function createRect(_childUI, _defWidthInNum, _defHeightInNum, _ADTexture, _cornerRadius, _background) {
     const rect = new GRect("rectangle")
 
     let width = _defWidthInNum ? _defWidthInNum : .8
     let height = _defHeightInNum ? _defHeightInNum : .5
     let defBackgroundC = "#27282a";
 
-    if(_childUI){
-        if(_childUI.length){
+    if (_childUI) {
+        if (_childUI.length) {
             _childUI.forEach(chld => rect.addControl(chld))
-        }else{
+        } else {
             rect.addControl(_childUI)
-        }        
+        }
     }
-    
+
     rect.width = width
     rect.height = height
-    rect.background = _background?_background:defBackgroundC
+    rect.background = _background ? _background : defBackgroundC
     rect.cornerRadius = _cornerRadius ? _cornerRadius : 3
 
-    if(_ADTexture) _ADTexture.addControl(rect)
-    
+    if (_ADTexture) _ADTexture.addControl(rect)
+
     return rect
 }
-module.createTxt = function(_textLabel, _fontSize, _color){
+function createTxt(_textLabel, _fontSize, _color) {
     const textBlock = new GUI.TextBlock("textblock", _textLabel)
     textBlock.color = _color ? _color : "white"
     textBlock.fontSize = _fontSize ? _fontSize : "40px"
     return textBlock
 }
-module.createCheckBox = function(_label, _ADTexture,_isChecked, _callBWhenValueChanged, _bxSize, _fontS, _background, _isBxPositionLeft){
+function createCheckBox(_label, _ADTexture, _isChecked, _callBWhenValueChanged, _bxSize, _fontS, _background, _isBxPositionLeft) {
     // let checkBx = new GUI.Checkbox.AddCheckBoxWithHeader("checkbox: ", _callBWhenValueChanged)
     const checkBx = new GCheckBx("checkbox")
     checkBx.isChecked = _isChecked
     checkBx.onIsCheckedChangedObservable.add(_callBWhenValueChanged)
-  
+
     const header = GUI.Checkbox.AddHeader(checkBx, _label, _fontS ? _fontS : "100px", {
         isHorizontal: true, controlFirst: _isBxPositionLeft
     })
@@ -115,15 +115,29 @@ module.createCheckBox = function(_label, _ADTexture,_isChecked, _callBWhenValueC
     checkBx.width = _bxSize ? _bxSize : defSize
     checkBx.height = _bxSize ? _bxSize : defSize
     checkBx.color = "white"
-    checkBx.background = _background?_background:defBackgroundC
+    checkBx.background = _background ? _background : defBackgroundC
 
 
-    if(_ADTexture){
+    if (_ADTexture) {
         _ADTexture.addControl(checkBx)
         _ADTexture.addControl(header)
     }
- 
-    return {checkBx,header}
+
+    return { checkBx, header }
+}
+function bguiTest() {
+    return log("bgui is working")
+}
+
+module.exports = {
+    bylonUIInit,
+    createBtn,
+    createRowsOfText,
+    createGrid,
+    createRect,
+    createTxt,
+    createCheckBox,
+    bguiTest
 }
 
 // EXAMPLE USAGE IN BABYLONJS
